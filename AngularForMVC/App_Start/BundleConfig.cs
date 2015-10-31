3f0527234c5b1aa63e7a048258774d68744be152
MT Dev <mobile.development@milwaukeetool.com>
2015-10-31 08:05:51 -0500

    Modified bundles

--- AngularForMVC/App_Start/BundleConfig.cs
+++ AngularForMVC/App_Start/BundleConfig.cs
@@ -26,6 +26,14 @@ namespace AngularForMVC
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/bundles/mini-spa/style").Include(
+                "",
+                ""));
+
+            bundles.Add(new ScriptBundle("~/bundles/mini-spa/style").Include(
+                "",
+                ""));
         }
     }
 }
